ab411b0dc5ff776e372c32e83e9f7ca4734d0f5f
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2024-03-02 11:42:01 +0300

    Add TextMeshPro

--- Assets/Sources/View/Ui/UiMainMenu.cs
+++ Assets/Sources/View/Ui/UiMainMenu.cs
@@ -1,7 +1,11 @@
+using Basketball_YG.Meta;
+
 namespace Basketball_YG.View.Ui
 {
     public class UiMainMenu
     {
-
+        private readonly ClickedCallback _startMath;
+        private readonly ClickedCallback _skinStore;
+        private readonly ClickedCallback _locationStore;
     }
 }
