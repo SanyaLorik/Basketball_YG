c62ebc1b0d90bb955d4a30d06a42c52da638b81b
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2024-04-05 18:14:52 +0300

    Fix skin selector

--- Assets/Sources/Core/Skin/BallSkinSelector.cs
+++ Assets/Sources/Core/Skin/BallSkinSelector.cs
@@ -1,14 +1,12 @@
 ﻿using Basketball_YG.Animation;
-using Basketball_YG.Config;
 using Basketball_YG.Model;
-using Zenject;
 
 namespace Basketball_YG.Core
 {
     public class BallSkinSelector : SkinSelector
     {
         public BallSkinSelector(
-            [InjectOptional(Optional = true, Id = GameConstants.BallSkinSelectingModel)] SkinSelectingModel model, 
+            BallSelectingModel model, 
             NotificationPopup notificationPopup) : base(model, notificationPopup)
         {
         }
--- Assets/Sources/Core/Skin/SiteSkinSelector.cs
+++ Assets/Sources/Core/Skin/SiteSkinSelector.cs
@@ -1,14 +1,12 @@
 ﻿using Basketball_YG.Animation;
-using Basketball_YG.Config;
 using Basketball_YG.Model;
-using Zenject;
 
 namespace Basketball_YG.Core
 {
     public class SiteSkinSelector : SkinSelector
     {
         public SiteSkinSelector(
-            [InjectOptional(Optional = true, Id = GameConstants.SiteSkinSelectingModel)] SkinSelectingModel model,
+            SiteSelectingModel model,
             NotificationPopup notificationPopup) : base(model, notificationPopup)
         {
         }
